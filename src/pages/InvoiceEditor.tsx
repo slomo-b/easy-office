b05d48fa4ceddadbad57a04a4ac841eb043b1e1b
Mo <contact@mo-freelancer.ch>
2025-11-17 23:56:11 +0100

    feat: Adjust invoice editor layout
    Refactor the layout of the InvoiceEditor component to improve responsiveness and ensure the preview pane has a fixed width on larger screens. This change uses flexbox properties to allow the form and editor sections to grow while keeping the preview section constrained.

--- src/pages/InvoiceEditor.tsx
+++ src/pages/InvoiceEditor.tsx
@@ -211,11 +211,11 @@ const InvoiceEditor = () => {
         </div>
         
         <main className="flex flex-col lg:flex-row gap-6">
-            <div className="lg:w-2/3 flex flex-col gap-6">
+            <div className="flex-1 flex flex-col gap-6">
               <InvoiceForm data={invoiceData} customers={customers} onDataChange={handleDataChange} defaultVatRate={settings.vatRate} />
               <HtmlEditor template={invoiceData.htmlTemplate} onTemplateChange={handleTemplateChange} />
             </div>
-            <div className="lg:w-1/3">
+            <div className="w-full lg:w-[420px] flex-shrink-0">
               <div className="sticky top-6">
                   <InvoicePreview processedTemplate={processedTemplate} onZoomClick={() => setIsZoomModalOpen(true)} />
               </div>
